27283bf28067e2fa7500fa7cbe985db2f9bcc688
FireML <firemlyt@gmail.com>
2024-08-30 20:15:55 +0100

    Respawn override

--- src/main/java/uk/firedev/firefly/modules/teleportation/TeleportListener.java
+++ src/main/java/uk/firedev/firefly/modules/teleportation/TeleportListener.java
@@ -1,10 +1,12 @@
 package uk.firedev.firefly.modules.teleportation;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
 public class TeleportListener implements Listener {
@@ -29,6 +31,14 @@ public class TeleportListener implements Listener {
     public void onTeleport(PlayerTeleportEvent event) {
         Player player = event.getPlayer();
         TeleportManager.getInstance().setLastLocation(player, event.getFrom());
+    }
+
+    @EventHandler
+    public void onRespawn(PlayerRespawnEvent event) {
+        Location customSpawnLocation = TeleportManager.getInstance().getSpawnLocation();
+        if (event.getPlayer().getRespawnLocation() == null && customSpawnLocation != null) {
+            event.setRespawnLocation(customSpawnLocation);
+        }
     }
 
 }
