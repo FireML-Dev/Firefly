f05f51da9cbf007be8165313425486d055fa9a1c
FireML <firemlyt@gmail.com>
2024-10-23 19:06:43 +0100

    Update DaisyLib to 2.1.0-SNAPSHOT

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
             library("paper-api", "io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
-            library("daisylib", "uk.firedev:DaisyLib:2.0.3-SNAPSHOT")
+            library("daisylib", "uk.firedev:DaisyLib:2.1.0-SNAPSHOT")
             library("vault", "com.github.MilkBowl:VaultAPI:1.7.1")
             library("placeholderapi", "me.clip:placeholderapi:2.11.6")
             library("miniplaceholders", "io.github.miniplaceholders:miniplaceholders-api:2.2.3")
