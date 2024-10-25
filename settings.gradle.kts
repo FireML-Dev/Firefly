be0ee037cb54908e102f56c2bee8671447bc9237
FireML <firemlyt@gmail.com>
2024-10-25 18:01:28 +0100

    Update Paper API to 1.21.3-R0.1-SNAPSHOT

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,13 +3,13 @@ rootProject.name = "Firefly"
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            library("paper-api", "io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
+            library("paper-api", "io.papermc.paper:paper-api:1.21.3-R0.1-SNAPSHOT")
             library("daisylib", "uk.firedev:DaisyLib:2.1.0-SNAPSHOT")
             library("vault", "com.github.MilkBowl:VaultAPI:1.7.1")
             library("placeholderapi", "me.clip:placeholderapi:2.11.6")
             library("miniplaceholders", "io.github.miniplaceholders:miniplaceholders-api:2.2.3")
 
-            plugin("shadow", "com.gradleup.shadow").version("8.3.0")
+            plugin("shadow", "com.gradleup.shadow").version("8.3.3")
             plugin("plugin-yml", "net.minecrell.plugin-yml.paper").version("0.6.0")
         }
     }
