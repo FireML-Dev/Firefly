e47eea5ffd1495aea81b96992e8e946422719552
FireML <fireml@firedev.uk>
2025-02-03 20:35:10 +0000

    Allow players to be specified in elevator give command

--- src/main/java/uk/firedev/firefly/modules/elevator/command/ElevatorCommand.java
+++ src/main/java/uk/firedev/firefly/modules/elevator/command/ElevatorCommand.java
@@ -1,8 +1,11 @@
 package uk.firedev.firefly.modules.elevator.command;
 
 import org.bukkit.FluidCollisionMode;
+import org.bukkit.entity.Player;
 import org.bukkit.util.RayTraceResult;
+import org.jetbrains.annotations.NotNull;
 import uk.firedev.daisylib.api.utils.ItemUtils;
+import uk.firedev.daisylib.command.arguments.PlayerArgument;
 import uk.firedev.daisylib.libs.commandapi.CommandAPICommand;
 import uk.firedev.firefly.modules.elevator.Elevator;
 import uk.firedev.firefly.modules.elevator.ElevatorConfig;
@@ -30,9 +33,14 @@ public class ElevatorCommand {
 
     private static CommandAPICommand getGiveBlockCommand() {
         return new CommandAPICommand("giveBlock")
+                .withArguments(PlayerArgument.create("target").setOptional(true))
                 .executesPlayer((player, arguments) -> {
-                    ItemUtils.giveItem(ElevatorModule.getInstance().getElevatorBlock(), player);
-                    ElevatorConfig.getInstance().getCommandGivenMessage().sendMessage(player);
+                    Player target = arguments.getUnchecked("target");
+                    if (target == null) {
+                        target = player;
+                    }
+                    ItemUtils.giveItem(ElevatorModule.getInstance().getElevatorBlock(), target);
+                    ElevatorConfig.getInstance().getCommandGivenMessage().sendMessage(target);
                 });
     }
 
