c45dcc99806ceaeb91c9aafce59f76e2ebaf46be
FireML <fireml@firedev.uk>
2025-03-08 13:37:44 +0000

    Clean up PlaytimeCommand

--- src/main/java/uk/firedev/firefly/modules/playtime/command/PlaytimeCommand.java
+++ src/main/java/uk/firedev/firefly/modules/playtime/command/PlaytimeCommand.java
@@ -5,12 +5,10 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import uk.firedev.daisylib.api.message.component.ComponentReplacer;
+import uk.firedev.daisylib.command.arguments.OfflinePlayerArgument;
 import uk.firedev.daisylib.libs.commandapi.CommandPermission;
 import uk.firedev.daisylib.libs.commandapi.CommandTree;
-import uk.firedev.daisylib.libs.commandapi.arguments.Argument;
-import uk.firedev.daisylib.libs.commandapi.arguments.AsyncOfflinePlayerArgument;
-import uk.firedev.daisylib.libs.commandapi.arguments.LiteralArgument;
-import uk.firedev.daisylib.libs.commandapi.arguments.LongArgument;
+import uk.firedev.daisylib.libs.commandapi.arguments.*;
 import uk.firedev.firefly.modules.playtime.PlaytimeConfig;
 import uk.firedev.firefly.modules.playtime.PlaytimeModule;
 
@@ -26,17 +24,17 @@ public class PlaytimeCommand {
     public static CommandTree getCommand() {
         if (command == null) {
             command = new CommandTree("playtime")
-                    .withPermission(CommandPermission.fromString("firefly.command.playtime"))
+                    .withPermission("firefly.command.playtime")
                     .withShortDescription("Check Playtime")
                     .withFullDescription("Check Playtime")
                     .executesPlayer(info -> {
                         sendPlaytime(info.sender(), info.sender());
                     })
                     .then(
-                        new AsyncOfflinePlayerArgument("target")
+                        OfflinePlayerArgument.createPlayedBefore("target")
                             .executes((sender, arguments) -> {
-                                CompletableFuture<OfflinePlayer> target = Objects.requireNonNull(arguments.getUnchecked("target"));
-                                target.thenAccept(offlinePlayer -> sendPlaytime(sender, offlinePlayer));
+                                OfflinePlayer target = Objects.requireNonNull(arguments.getUnchecked("target"));
+                                sendPlaytime(sender, target);
                             })
                     )
                     .then(getSetPlaytimeBranch());
@@ -49,7 +47,10 @@ public class PlaytimeCommand {
                 "player", Objects.requireNonNullElse(playerToCheck.getName(), "N/A"),
                 "playtime", PlaytimeModule.getInstance().getTimeFormatted(playerToCheck)
         );
-        PlaytimeConfig.getInstance().getCheckPlaytimeMessage().applyReplacer(replacer).sendMessage(sender);
+        PlaytimeConfig.getInstance().getCheckPlaytimeMessage()
+            .replace("player", Objects.requireNonNull(playerToCheck.getName()))
+            .replace("playtime", PlaytimeModule.getInstance().getTimeFormatted(playerToCheck))
+            .applyReplacer(replacer).sendMessage(sender);
     }
 
     // /playtime set Branch
@@ -58,15 +59,13 @@ public class PlaytimeCommand {
         return new LiteralArgument("set")
                 .withPermission("firefly.command.playtime.admin")
                 .thenNested(
-                        new AsyncOfflinePlayerArgument("target"),
+                        OfflinePlayerArgument.createPlayedBefore("target"),
                         new LongArgument("playtime", 0)
                                 .executes((sender, arguments) -> {
-                                    CompletableFuture<OfflinePlayer> future = Objects.requireNonNull(arguments.getUnchecked("target"));
+                                    OfflinePlayer target = Objects.requireNonNull(arguments.getUnchecked("target"));
                                     long newPlaytime = Objects.requireNonNull(arguments.getUnchecked("playtime"));
-                                    future.thenAccept(target -> {
-                                        PlaytimeModule.getInstance().setTime(target, newPlaytime);
-                                        sendSetPlaytimeMessage(sender, target);
-                                    });
+                                    PlaytimeModule.getInstance().setTime(target, newPlaytime);
+                                    sendSetPlaytimeMessage(sender, target);
                                 })
                 );
     }
