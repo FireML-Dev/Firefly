9ae7177c99b45ab795b69b9ab589f9c322658c31
FireML <fireml@firedev.uk>
2025-03-27 19:22:22 +0000

    Set plugin version to 1.3.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
 }
 
 group = "uk.firedev"
-version = "1.2.0-SNAPSHOT"
+version = "1.3.0-SNAPSHOT"
 description = "A collection of helpful server features."
 java.sourceCompatibility = JavaVersion.VERSION_21
 
