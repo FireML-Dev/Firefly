5af6c0d254ac1f6fe3182c65adce405be5794017
FireML <fireml@firedev.uk>
2025-10-31 02:29:26 +0000

    Switch PrefixCommand to Brigadier

--- src/main/java/uk/firedev/firefly/modules/titles/command/PrefixCommand.java
+++ src/main/java/uk/firedev/firefly/modules/titles/command/PrefixCommand.java
@@ -1,9 +1,13 @@
 package uk.firedev.firefly.modules.titles.command;
 
+import com.mojang.brigadier.builder.ArgumentBuilder;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
 import net.kyori.adventure.text.Component;
-import uk.firedev.daisylib.libs.commandapi.CommandTree;
-import uk.firedev.daisylib.libs.commandapi.arguments.Argument;
-import uk.firedev.daisylib.libs.commandapi.arguments.StringArgument;
+import org.bukkit.entity.Player;
+import uk.firedev.daisylib.command.CommandUtils;
+import uk.firedev.firefly.modules.command.Command;
 import uk.firedev.firefly.modules.titles.TitleConfig;
 import uk.firedev.firefly.modules.titles.TitleModule;
 import uk.firedev.firefly.modules.titles.gui.PrefixGui;
@@ -14,28 +18,38 @@ import java.awt.*;
 
 public class PrefixCommand {
 
-    public static CommandTree getCommand() {
-        return new CommandTree("prefix")
-            .withPermission("firefly.command.prefix")
-            .withHelp("Manage Prefix", "Manage Prefix")
-            .executesPlayer(info -> {
-                new PrefixGui(info.sender()).open();
+    public LiteralCommandNode<CommandSourceStack> get() {
+        return Commands.literal("prefix")
+            .requires(stack -> stack.getSender().hasPermission("firefly.command.prefix"))
+            .executes(context -> {
+                Player player = CommandUtils.requirePlayer(context.getSource());
+                if (player == null) {
+                    return 1;
+                }
+                new PrefixGui(player).open();
+                return 1;
             })
-            .then(getDisplayBranch());
+            .then(display())
+            .build();
     }
 
 
-    private static Argument<String> getDisplayBranch() {
-        return new StringArgument("display")
-                .executesPlayer((player, arguments) -> {
-                    ComponentSingleMessage prefix = ComponentMessage.componentMessage(TitleModule.getInstance().getPlayerPrefix(player));
-                    if (prefix.isEmpty()) {
-                        prefix = ComponentMessage.componentMessage(Component.text("None"));
-                    }
-                    TitleConfig.getInstance().getPrefixDisplayMessage()
-                            .replace("{player-prefix}", prefix)
-                            .send(player);
-                });
+    private static ArgumentBuilder<CommandSourceStack, ?> display() {
+        return Commands.literal("display")
+            .executes(context -> {
+                Player player = CommandUtils.requirePlayer(context.getSource());
+                if (player == null) {
+                    return 1;
+                }
+                ComponentSingleMessage prefix = ComponentMessage.componentMessage(TitleModule.getInstance().getPlayerPrefix(player));
+                if (prefix.isEmpty()) {
+                    prefix = ComponentMessage.componentMessage(Component.text("None"));
+                }
+                TitleConfig.getInstance().getPrefixDisplayMessage()
+                    .replace("{player-prefix}", prefix)
+                    .send(player);
+                return 1;
+            });
     }
 
 }
