4f0886f28f4018bee13119eca2e3dc4196abd197
FireML <fireml@firedev.uk>
2025-10-31 02:56:31 +0000

    Switch SpawnCommand to Brigadier

--- src/main/java/uk/firedev/firefly/modules/teleportation/commands/spawn/SpawnCommand.java
+++ src/main/java/uk/firedev/firefly/modules/teleportation/commands/spawn/SpawnCommand.java
@@ -1,28 +1,38 @@
 package uk.firedev.firefly.modules.teleportation.commands.spawn;
 
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
 import org.bukkit.entity.Player;
-import uk.firedev.daisylib.libs.commandapi.CommandTree;
+import uk.firedev.daisylib.command.CommandUtils;
+import uk.firedev.daisylib.command.arguments.PlayerArgument;
 import uk.firedev.firefly.modules.teleportation.TeleportModule;
 
 import java.util.Objects;
 
 public class SpawnCommand {
 
-    public static CommandTree getCommand() {
-        return new CommandTree("spawn")
-            .withPermission("firefly.command.spawn")
-            .withHelp("Teleport to spawn", "Teleport to spawn")
-            .executesPlayer(info -> {
-                TeleportModule.getInstance().sendToSpawn(false, info.sender(), true);
+    public LiteralCommandNode<CommandSourceStack> get() {
+        return Commands.literal("spawn")
+            .requires(stack -> stack.getSender().hasPermission("firefly.command.spawn"))
+            .executes(context -> {
+                Player player = CommandUtils.requirePlayer(context.getSource());
+                if (player == null) {
+                    return 1;
+                }
+                TeleportModule.getInstance().sendToSpawn(false, player, true);
+                return 1;
             })
             .then(
-                uk.firedev.daisylib.command.arguments.PlayerArgument.create("target")
-                    .withPermission("firefly.command.spawn.other")
-                    .executes(info -> {
-                        Player target = Objects.requireNonNull(info.args().getUnchecked("target"));
-                        TeleportModule.getInstance().sendToSpawn(false, target, info.sender(), true);
+                Commands.argument("target", PlayerArgument.create())
+                    .requires(stack -> stack.getSender().hasPermission("firefly.command.spawn.other"))
+                    .executes(context -> {
+                        Player target = context.getArgument("target", Player.class);
+                        TeleportModule.getInstance().sendToSpawn(false, target, context.getSource().getSender(), true);
+                        return 1;
                     })
-            );
+            )
+            .build();
     }
 
 }
