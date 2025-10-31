5c6db0f61f736bab0fc77c1ca0c9efd0135c0b87
FireML <fireml@firedev.uk>
2025-10-31 03:42:21 +0000

    Switch MessageCommand to Brigadier

--- src/main/java/uk/firedev/firefly/modules/messaging/command/MessageCommand.java
+++ src/main/java/uk/firedev/firefly/modules/messaging/command/MessageCommand.java
@@ -1,40 +1,46 @@
 package uk.firedev.firefly.modules.messaging.command;
 
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
 import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import uk.firedev.daisylib.command.CommandUtils;
 import uk.firedev.daisylib.command.arguments.PlayerArgument;
-import uk.firedev.daisylib.libs.commandapi.CommandTree;
-import uk.firedev.daisylib.libs.commandapi.arguments.GreedyStringArgument;
 import uk.firedev.firefly.modules.messaging.MessagingConfig;
 import uk.firedev.firefly.modules.messaging.MessagingModule;
 import uk.firedev.firefly.modules.nickname.NicknameModule;
 import uk.firedev.firefly.utils.StringUtils;
 import uk.firedev.daisylib.libs.messagelib.message.ComponentMessage;
 
-import java.util.Objects;
-
 public class MessageCommand {
 
-    public static CommandTree getCommand() {
-        MessagingConfig config = MessagingConfig.getInstance();
-        return new CommandTree(config.getMessageCommandName())
-            .withAliases(config.getMessageCommandAliases())
-            .withShortDescription("Send a message to another player.")
-            .withPermission(MessagingModule.MESSAGE_PERMISSION)
-            .thenNested(
-                PlayerArgument.create("target"),
-                new GreedyStringArgument("message")
-                    .executesPlayer(info -> {
-                        Player target = Objects.requireNonNull(info.args().getUnchecked("target"));
-                        String str = Objects.requireNonNull(info.args().getUnchecked("message"));
-
-                        sendMessage(info.sender(), target, str);
-                    })
-            );
+    // TODO aliases.
+    public LiteralCommandNode<CommandSourceStack> get() {
+        return Commands.literal(MessagingConfig.getInstance().getMessageCommandName())
+            .requires(stack -> stack.getSender().hasPermission(MessagingModule.REPLY_PERMISSION))
+            .then(
+                Commands.argument("target", PlayerArgument.create())
+                    .then(
+                        Commands.argument("message", StringArgumentType.greedyString())
+                            .executes(context -> {
+                                Player player = CommandUtils.requirePlayer(context.getSource());
+                                if (player == null) {
+                                    return 1;
+                                }
+                                Player target = context.getArgument("target", Player.class);
+                                String string = context.getArgument("message", String.class);
+                                sendMessage(player, target, string);
+                                return 1;
+                            })
+                    )
+            )
+            .build();
     }
 
-    private static void sendMessage(@NotNull Player sender, @NotNull Player target, @NotNull String str) {
+    private void sendMessage(@NotNull Player sender, @NotNull Player target, @NotNull String str) {
         Component message = StringUtils.getColorOnlyComponent(str);
         ComponentMessage msg = MessagingConfig.getInstance().getMessageFormat()
             .replace("{sender}", getNickname(sender))
@@ -46,7 +52,7 @@ public class MessageCommand {
         MessagingModule.getInstance().setLastMessage(target.getUniqueId(), sender.getUniqueId());
     }
 
-    private static @NotNull Component getNickname(@NotNull Player player) {
+    private @NotNull Component getNickname(@NotNull Player player) {
         if (NicknameModule.getInstance().isLoaded()) {
             return NicknameModule.getInstance().getNickname(player);
         }
