39209418fe44e3a6bcfc1cb963c6348d2e5efed8
FireML <fireml@firedev.uk>
2025-10-31 02:47:00 +0000

    Switch BackCommand to Brigadier

--- src/main/java/uk/firedev/firefly/modules/teleportation/commands/back/BackCommand.java
+++ src/main/java/uk/firedev/firefly/modules/teleportation/commands/back/BackCommand.java
@@ -1,36 +1,43 @@
 package uk.firedev.firefly.modules.teleportation.commands.back;
 
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import uk.firedev.daisylib.command.CommandUtils;
 import uk.firedev.daisylib.command.arguments.PlayerArgument;
-import uk.firedev.daisylib.libs.commandapi.CommandTree;
 import uk.firedev.firefly.modules.teleportation.TeleportConfig;
 import uk.firedev.firefly.modules.teleportation.TeleportModule;
 import uk.firedev.firefly.utils.TeleportWarmup;
 
-import java.util.Objects;
-
 public class BackCommand {
 
-    public static CommandTree getCommand() {
-        return new CommandTree("back")
-            .withPermission("firefly.command.back")
-            .withHelp("Teleport to your last location", "Teleport to your last location")
-            .executesPlayer(info -> {
-                teleportPlayer(info.sender(), info.sender());
+    public LiteralCommandNode<CommandSourceStack> get() {
+        return Commands.literal("back")
+            .requires(stack -> stack.getSender().hasPermission("firefly.command.back"))
+            .executes(context -> {
+                Player player = CommandUtils.requirePlayer(context.getSource());
+                if (player == null) {
+                    return 1;
+                }
+                teleportPlayer(player, player);
+                return 1;
             })
             .then(
-                PlayerArgument.create("target")
-                    .executes(info -> {
-                        Player target = Objects.requireNonNull(info.args().getUnchecked("target"));
-                        teleportPlayer(info.sender(), target);
+                Commands.argument("target", PlayerArgument.create())
+                    .executes(context -> {
+                        Player target = context.getArgument("target", Player.class);
+                        teleportPlayer(context.getSource().getSender(), target);
+                        return 1;
                     })
-            );
+            )
+            .build();
     }
 
-    private static void teleportPlayer(@NotNull CommandSender sender, @NotNull Player target) {
+    private void teleportPlayer(@NotNull CommandSender sender, @NotNull Player target) {
         Location location = TeleportModule.getInstance().getLastLocation(target);
         if (location == null) {
             TeleportConfig.getInstance().getLocationInvalidMessage().send(sender);
