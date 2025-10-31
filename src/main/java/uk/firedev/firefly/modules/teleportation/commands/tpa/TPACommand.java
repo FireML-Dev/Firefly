00433f201371623f94107da12e3fb0f4138c44a0
FireML <fireml@firedev.uk>
2025-10-31 02:52:01 +0000

    Switch TPACommand to Brigadier

--- src/main/java/uk/firedev/firefly/modules/teleportation/commands/tpa/TPACommand.java
+++ src/main/java/uk/firedev/firefly/modules/teleportation/commands/tpa/TPACommand.java
@@ -1,26 +1,32 @@
 package uk.firedev.firefly.modules.teleportation.commands.tpa;
 
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
 import org.bukkit.entity.Player;
+import uk.firedev.daisylib.command.CommandUtils;
 import uk.firedev.daisylib.command.arguments.PlayerArgument;
-import uk.firedev.daisylib.libs.commandapi.CommandTree;
 import uk.firedev.firefly.modules.teleportation.tpa.TPAHandler;
 import uk.firedev.firefly.modules.teleportation.tpa.TPARequest;
 
-import java.util.Objects;
-
 public class TPACommand {
 
-    public static CommandTree getCommand() {
-        return new CommandTree("tpa")
-            .withPermission("firefly.command.tpa")
-            .withHelp("Request teleports.", "Request teleports.")
+    public LiteralCommandNode<CommandSourceStack> get() {
+        return Commands.literal("tpa")
+            .requires(stack -> stack.getSender().hasPermission("firefly.command.tpa"))
             .then(
-                PlayerArgument.create("target")
-                    .executesPlayer(info -> {
-                        Player target = Objects.requireNonNull(info.args().getUnchecked("target"));
-                        TPAHandler.getInstance().sendRequest(target, info.sender(), TPARequest.TPADirection.SENDER_TO_TARGET);
+                Commands.argument("target", PlayerArgument.create())
+                    .executes(context -> {
+                        Player player = CommandUtils.requirePlayer(context.getSource());
+                        if (player == null) {
+                            return 1;
+                        }
+                        Player target = context.getArgument("target", Player.class);
+                        TPAHandler.getInstance().sendRequest(target, player, TPARequest.TPADirection.SENDER_TO_TARGET);
+                        return 1;
                     })
-            );
+            )
+            .build();
     }
 
 }
