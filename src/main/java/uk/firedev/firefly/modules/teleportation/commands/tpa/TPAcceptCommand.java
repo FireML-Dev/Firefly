7818ea542592e8976911138ea4f6604b134b0b4c
FireML <fireml@firedev.uk>
2025-10-31 02:52:42 +0000

    Switch TPAcceptCommand to Brigadier

--- src/main/java/uk/firedev/firefly/modules/teleportation/commands/tpa/TPAcceptCommand.java
+++ src/main/java/uk/firedev/firefly/modules/teleportation/commands/tpa/TPAcceptCommand.java
@@ -1,17 +1,27 @@
 package uk.firedev.firefly.modules.teleportation.commands.tpa;
 
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import org.bukkit.entity.Player;
+import uk.firedev.daisylib.command.CommandUtils;
 import uk.firedev.daisylib.libs.commandapi.CommandTree;
 import uk.firedev.firefly.modules.teleportation.tpa.TPAHandler;
 
 public class TPAcceptCommand {
 
-    public static CommandTree getCommand() {
-        return new CommandTree("tpaccept")
-            .withPermission("firefly.command.tpa")
-            .withHelp("Accept tpa requests.", "Accept tpa requests.")
-            .executesPlayer(info -> {
-                TPAHandler.getInstance().acceptRequest(info.sender());
-            });
+    public LiteralCommandNode<CommandSourceStack> get() {
+        return Commands.literal("tpaccept")
+            .requires(stack -> stack.getSender().hasPermission("firefly.command.tpa"))
+            .executes(context -> {
+                Player player = CommandUtils.requirePlayer(context.getSource());
+                if (player == null) {
+                    return 1;
+                }
+                TPAHandler.getInstance().acceptRequest(player);
+                return 1;
+            })
+            .build();
     }
 
 }
